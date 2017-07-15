9194f94b721c21d66714fb4caf098a8e1498d499
Abel Guzman <abel.aguzmans@gmail.com>
2017-07-14 23:00:39 -0400

    read from zabbix and sugar DB done

---
+++ web/sync_config.dist.php
@@ -0,0 +1,14 @@
+<?php
+/* Database configs */
+$database_sugar_host = "127.0.0.1";
+$database_sugar_port = "";
+$database_sugar_name_db = "sugarcrm_db";
+$database_sugar_user = "user_sugar";
+$database_sugar_password = "password";
+
+/* Database configs */
+$database_zabbix_host = "127.0.0.1";
+$database_zabbix_port = "";
+$database_zabbix_name_db = "zabbix_db";
+$database_zabbix_user = "user_zabbix";
+$database_zabbix_password = "password";
--- web/sync_sugar.php
+++ web/sync_sugar.php
@@ -2,49 +2,68 @@
 // Database config
 require_once './sync_config.php';
 // Count Devices
-/*  */
-$sqlCountTotalDevices = "SELECT 
-        COUNT(8) 
-        FROM 
-            srvrs_servers JOIN srvrs_servers_cstm ON srvrs_servers_cstm.id_c = srvrs_servers.id
-        WHERE
-            deleted=:deleted AND backuped_c =:backuped_c
-            AND status_active_c =:status_active_c;";
+/* Removed from DB:
+        id,
+        deleted,
+        backuped_c,
+        status_active_c  
+ */
+$sqlGetTotalDevices = "SELECT 
+        name as host
+    FROM 
+        srvrs_servers JOIN srvrs_servers_cstm ON srvrs_servers_cstm.id_c = srvrs_servers.id
+    WHERE
+        deleted=:deleted AND backuped_c =:backuped_c
+        AND status_active_c =:status_active_c;";
+// limit 2;
 /* Execute query */
-$db = new PDO('mysql:host='.$database_sugar_host
-        .';port:'.$database_sugar_port.';dbname='
-        .$database_sugar_name_db.';charset=utf8mb4'
+$opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$connectionChain = 'mysql:host='.$database_sugar_host
+        .';port='.$database_sugar_port.';dbname='
+        .$database_sugar_name_db.';charset=utf8mb4';
+
+$db_sugar = new PDO($connectionChain
         , $database_sugar_user
         , $database_sugar_password);
-$stmt = $db->prepare($sqlCountTotalDevices);
-$stmt->execute(array(':deleted' => '0'
+// , $opt
+$stmt_sugar = $db_sugar->prepare($sqlGetTotalDevices);
+$stmt_sugar->execute(array(':deleted' => '0'
     , ':backuped_c' => 1
     , ':status_active_c' => 'Active'));
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//PDO::FETCH_ASSOC
+$rowsSugar = $stmt_sugar->fetchAll(PDO::FETCH_COLUMN, 0);
+//var_dump($rows_sugar);
 
 /* Zabbix production groups */
-/*
+//removed from DB: , status
+$sqlGetTotalZabbixProd = "SELECT host
+    FROM hosts h
+    JOIN hosts_groups hg ON h.hostid = hg.hostid
+    WHERE (hg.groupid =:groupid24x7 OR hg.groupid =:groupid9x5)
+    AND h.status =:status;";
+// limit 5
+$db_zabbix = new PDO('mysql:host='.$database_zabbix_host
+        .';port='.$database_zabbix_port.';dbname='
+        .$database_zabbix_name_db.';charset=utf8mb4'
+        , $database_zabbix_user
+        , $database_zabbix_password);
+$stmt_zabbix = $db_zabbix->prepare($sqlGetTotalZabbixProd);
+$stmt_zabbix->execute(array(':status' => '0'
+    , ':groupid24x7' => 23
+    , ':groupid9x5' => 46));
+$rowsZabbix = $stmt_zabbix->fetchAll(PDO::FETCH_COLUMN, 0);
+//PDO::FETCH_ASSOC
+//var_dump($rows_zabbix);
+function compareHostNameZabbix($a, $b)
+{
+  return strnatcmp($a['host'], $b['host']);
+}
 
-SELECT host, status
-FROM hosts h
-JOIN hosts_groups hg ON h.hostid = hg.hostid
-WHERE (hg.groupid = 23 OR hg.groupid = 46)
-AND h.status = 0;
+print_r($rowsSugar);
+print_r($rowsZabbix);
 
-*/
-
-/* Get zerver names inj barches from sugarcrm DB */
-$sqlSpecificbatch = "SELECT 
-            id,
-            name,
-            deleted,
-            backuped_c,
-            status_active_c
-        FROM 
-            srvrs_servers JOIN srvrs_servers_cstm ON srvrs_servers_cstm.id_c = srvrs_servers.id
-        WHERE
-            deleted = 0
-            AND backuped_c = 1
-            AND status_active_c = 'Active'
-        LIMIT 8;";
-
+$resultInsertsect = array_intersect($rowsZabbix, $rowsSugar);
