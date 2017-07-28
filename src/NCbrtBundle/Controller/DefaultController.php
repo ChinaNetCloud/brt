3cbfe37389c46180694c9b2964e5a2771c946d87
Abel Guzman <abel.aguzmans@gmail.com>
2017-07-28 19:05:17 +0800

    Merge remote-tracking branch 'remotes/origin/Abel-dev'
    Conflicts:
    	.gitignore
    	index.php
    	log/api.log
    	resource/api.sql

--- src/NCbrtBundle/Controller/DefaultController.php
+++ src/NCbrtBundle/Controller/DefaultController.php
@@ -4,6 +4,16 @@ namespace NCbrtBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use \Symfony\Component\HttpFoundation\Request;
+use \Symfony\Component\HttpFoundation\Response;
+
+use NCbrtBundle\Entity\SrvrsServers;
+use NCbrtBundle\Entity\NcBackupEvents;
+
+use NCbrtBundle\Tools\Toools;
+
 
 class DefaultController extends Controller
 {
@@ -12,10 +22,65 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        /*Select everything that is younger than a week by default, 
-         * clearly say the time and give options to change filter .
-         */
-        
+        $em = $this->getDoctrine()->getManager();
+        $selectAll = $em->getRepository('NCbrtBundle:SrvrsServers')
+                ->createQueryBuilder('s')
+                ->innerJoin('s.NcBackupEvents', 'n')
+                ->where('s.statusActive = :statusActive')
+                ->setParameter('statusActive', 0)
+                ->setMaxResults(3);
         return $this->render('NCbrtBundle:Default:index.html.twig');
+    }
+    
+    /**
+     * @Route("/backup/")
+     */
+    public function insertAction(Request $request){
+
+        /* Get data from post request */
+        $content = $request->request->all();
+
+        /* Check if server is already in DB */
+        $em = $this->getDoctrine()->getManager();
+
+        $selectAll = $em->getRepository('NCbrtBundle:SrvrsServers')
+                ->createQueryBuilder('s')
+                ->where('s.name = :server_name')
+                ->setParameter('server_name', $content['srvname'])
+                ->getQuery()
+                ->getResult();
+
+        /* Add Server if not in DB */
+        $serverEntity = new SrvrsServers();
+        if (empty($selectAll)) {
+            $serverEntity->setName($content['srvname']);
+            $serverEntity->setStatusActive(0);
+            $em->persist($serverEntity);
+            $em->flush();
+        } else {
+//            $serverEntity= Toools::array_to_object($selectAll);
+//            $serverEntity->fro
+        }
+        /* At this point the server is already on DB I should have the ID */
+        
+        /* Create NEW backup event */
+        if (!empty((array($serverEntity)))){
+            $backupEvent = new NcBackupEvents();
+            $backupEvent->setSrvrsServers($serverEntity);
+            $backupEvent->setBackupmethod($content['bckmethod']);
+            $backupEvent->setBackupsize($content['size']);
+            $backupEvent->setLog($content['log']);
+            $backupEvent->setSuccess($content['result']);
+            $backupEvent->setBackupType($content['destination']);
+            $em->persist($serverEntity);
+            $em->flush();
+            
+        } else {
+            return new Response('NO Server selected after post');
+        }
+
+        
+        /* Response with result 200 plus message*/
+        return new Response('New Backup result added for server: ' . $serverEntity->getName());
     }
 }
