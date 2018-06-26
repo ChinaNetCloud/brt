6920e4f8bff7ad8be85df807891b36fd2ebbd6d0
Aluisco M. Ricardo Mastrapa <aluisco2005@gmail.com>
2018-06-26 11:09:15 +0900

    Support for autentication

--- src/NCbrtBundle/Entity/User.php
+++ src/NCbrtBundle/Entity/User.php
@@ -1,136 +1,26 @@
 <?php
 
-/*
- * Autho: Abel Guzman Sanchez
- * 2017
- * Apache license 2.0
- */
-
 namespace NCbrtBundle\Entity;
 
-
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
-use Doctrine\ORM\Mapping\ManyToOne;
-use Doctrine\ORM\Mapping\JoinColumn;
 /**
- * Description of User
- *
- * @author cncuser
- */
-/**
- * User
- *
- * @ORM\Table(name="user")
  * @ORM\Entity
+ * @ORM\Table(name="fos_user")
  */
-class User {
+class User extends BaseUser
+{
     /**
-     * @var integer
-     *
-     * @ORM\Column(name="id", type="integer", nullable=false)
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
-    
-    /**
-     * @var string
-     * @ORM\Column(name="email", type="string")
-     */
-    private $email;    
-    
-    /**
-     * @var string
-     * @ORM\Column(name="nickname", type="string", nullable=true)
-     */
-    private $nickname;
-     
-    /**
-     * @ManyToOne(targetEntity="Role", cascade={"persist", "remove"})
-     * @JoinColumn(name="role_name_id", referencedColumnName="name")
-     */
-    private $role;
+    protected $id;
 
-    /**
-     * Get id
-     *
-     * @return integer
-     */
-    public function getId()
+    public function __construct()
     {
-        return $this->id;
-    }
-
-    /**
-     * Set email
-     *
-     * @param string $email
-     *
-     * @return User
-     */
-    public function setEmail($email)
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-    /**
-     * Get email
-     *
-     * @return string
-     */
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    /**
-     * Set nickname
-     *
-     * @param string $nickname
-     *
-     * @return User
-     */
-    public function setNickname($nickname)
-    {
-        $this->nickname = $nickname;
-
-        return $this;
-    }
-
-    /**
-     * Get nickname
-     *
-     * @return string
-     */
-    public function getNickname()
-    {
-        return $this->nickname;
-    }
-
-    /**
-     * Set role
-     *
-     * @param \NCbrtBundle\Entity\Role $role
-     *
-     * @return User
-     */
-    public function setRole(\NCbrtBundle\Entity\Role $role = null)
-    {
-        $this->role = $role;
-
-        return $this;
-    }
-
-    /**
-     * Get role
-     *
-     * @return \NCbrtBundle\Entity\Role
-     */
-    public function getRole()
-    {
-        return $this->role;
+        parent::__construct();
+        // your own logic
     }
 }
