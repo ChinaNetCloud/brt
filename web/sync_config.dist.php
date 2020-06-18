678ff902c4063efd3095ae64415f9d23fcda707e
Abel <abel.aguzmans@gmail.com>
2020-06-18 09:00:04 +0800

    Update sync_config.dist.php

--- web/sync_config.dist.php
+++ web/sync_config.dist.php
@@ -4,18 +4,18 @@ $database_sugar_host = "127.0.0.1";
 $database_sugar_port = "";
 $database_sugar_name_db = "sugarcrm_db";
 $database_sugar_user = "user_sugar";
-$database_sugar_password = "password";
+$database_sugar_password = "";
 
 /* Database configs */
 $database_zabbix_host = "127.0.0.1";
 $database_zabbix_port = "";
 $database_zabbix_name_db = "zabbix_db";
 $database_zabbix_user = "user_zabbix";
-$database_zabbix_password = "password";
+$database_zabbix_password = "";
 
 /* Database configs BRT*/
 $database_brt_host = "127.0.0.1";
 $database_brt_port = "3306";
 $database_brt_name_db = "brt001";
 $database_brt_user = "user_drt_db"; // Can use the same as synfony
-$database_brt_password = "password";
+$database_brt_password = "";
